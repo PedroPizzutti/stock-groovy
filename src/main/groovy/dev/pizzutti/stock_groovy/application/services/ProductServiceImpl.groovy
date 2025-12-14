125beefffeef391d20e394b7c9c37f8ea58a110f
Pedro Pizzutti <pedropizzutti@gmail.com>
2025-12-14 16:57:11 -0300

    :sparkles: feat: melhora das regras de negócio do caso de uso de stock

--- src/main/groovy/dev/pizzutti/stock_groovy/application/services/ProductServiceImpl.groovy
+++ src/main/groovy/dev/pizzutti/stock_groovy/application/services/ProductServiceImpl.groovy
@@ -5,6 +5,7 @@ import dev.pizzutti.stock_groovy.domain.exception.ProductException
 import dev.pizzutti.stock_groovy.domain.ports.input.ProductService
 import dev.pizzutti.stock_groovy.domain.ports.output.ProductRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class ProductServiceImpl implements ProductService {
@@ -16,37 +17,43 @@ class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    @Transactional
     Product create(Product product) {
-        if (productRepository.findByCodBar(product.codBar).isPresent()) {
-            throw new ProductException(["Product with 'codBar' '${product.codBar}' already exists"])
-        }
-        return productRepository.save(product)
+        validateUniqueCodBar(product.codBar)
+        productRepository.save(product)
     }
 
     @Override
+    @Transactional
     Product update(Product product) {
-        def persistedProduct = productRepository.findById(product.id)
-                .orElseThrow { new ProductException(["Product with id '${product.id}' not found"]) }
-
-        if ((persistedProduct.codBar != product.codBar) && (productRepository.findByCodBar(product.codBar).isPresent())) {
-            throw new ProductException(["Product with 'codBar' '${product.codBar}' already exists"])
+        def persistedProduct = getById(product.id)
+        def isChangingCodBar = persistedProduct.codBar != product.codBar;
+        if (isChangingCodBar) {
+            validateUniqueCodBar(product.codBar)
         }
-        return productRepository.save(product)
+        productRepository.save(product)
     }
 
     @Override
     Product getById(UUID id) {
-        return productRepository.findById(id)
-                .orElseThrow({ new ProductException(["Product with id '${id}' not found"]) })
+         productRepository.findById(id).orElseThrow({ new ProductException(["Product with id '${id}' not found"]) })
     }
 
     @Override
     List<Product> listAll() {
-        return productRepository.findAll()
+        productRepository.findAll()
     }
 
     @Override
+    @Transactional
     void delete(UUID id) {
-        productRepository.delete(id)
+        def persistedProduct = getById(id)
+        productRepository.delete(persistedProduct.id)
+    }
+
+    private void validateUniqueCodBar(String codBar) {
+        if (productRepository.findByCodBar(codBar).isPresent()) {
+            throw new ProductException(["Product with 'codBar' '${codBar}' already exists"])
+        }
     }
 }
