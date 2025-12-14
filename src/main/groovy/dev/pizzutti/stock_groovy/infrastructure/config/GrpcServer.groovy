22d782aee5c724eb0114a44820bdd1f41ef57330
Pedro Pizzutti <pedropizzutti@gmail.com>
2025-12-14 17:08:45 -0300

    :sparkles: feat: log no gRPC server

--- src/main/groovy/dev/pizzutti/stock_groovy/infrastructure/config/GrpcServer.groovy
+++ src/main/groovy/dev/pizzutti/stock_groovy/infrastructure/config/GrpcServer.groovy
@@ -2,20 +2,22 @@ package dev.pizzutti.stock_groovy.infrastructure.config
 
 import dev.pizzutti.stock_groovy.infrastructure.adapters.input.grpc.exception.GrpcExceptionHandler
 import dev.pizzutti.stock_groovy.infrastructure.adapters.input.grpc.services.ProductGrpcImpl
+import groovy.util.logging.Slf4j
 import io.grpc.Server
 import io.grpc.ServerBuilder
 import io.grpc.protobuf.services.ProtoReflectionService
 import org.springframework.context.annotation.Configuration
 
 import javax.annotation.PostConstruct
-import java.time.LocalDateTime
 
 @Configuration
+@Slf4j
 class GrpcServer {
 
-    private Server server
-    private GrpcExceptionHandler exceptionHandler
-    private ProductGrpcImpl productGrpcImpl
+    final static PORT = 6565
+    Server server
+    GrpcExceptionHandler exceptionHandler
+    ProductGrpcImpl productGrpcImpl
 
     GrpcServer(GrpcExceptionHandler exceptionHandler,
                ProductGrpcImpl productGrpcImpl) {
@@ -26,18 +28,21 @@ class GrpcServer {
     @PostConstruct
     void start() {
         server = ServerBuilder
-                .forPort(6565)
+                .forPort(PORT)
                 .intercept(exceptionHandler)
                 .addService(productGrpcImpl)
                 .addService(ProtoReflectionService.newInstance())
                 .build()
+
         server.start()
-        println LocalDateTime.now().toString() + "  INFO --- gRPC Server on port 6565"
+        log.info("gRPC server started on port (${PORT})")
+
+        final Server serverRef = server
 
         Runtime.runtime.addShutdownHook(new Thread({
-            server?.shutdown()
-            println LocalDateTime.now().toString() + "  INFO --- gRPC Server down"
-        }))
+            serverRef?.shutdown()
+            log.info("gRPC Server down")
+        }, "grpc-shutdown-hook"))
     }
 
 }
