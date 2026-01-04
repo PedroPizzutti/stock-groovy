a3541070e360478810c54aa9daaf4171beef76b9
Pedro Pizzutti <pedropizzutti@gmail.com>
2026-01-04 10:27:17 -0300

    feat: criado projeto de vendas

---
+++ sales/src/test/java/dev/pizzutti/sales_java/SalesJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pizzutti.sales_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
